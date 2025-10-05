b07c834e83c6e72a36c82431843d42a66214c787
Shahar Mazor <103638798+Django149@users.noreply.github.com>
2025-10-05 22:24:38 +0300

    Add RTL support (#5609)

--- web/src/app/chat/message/messageComponents/markdownUtils.tsx
+++ web/src/app/chat/message/messageComponents/markdownUtils.tsx
@@ -97,15 +97,16 @@ export const renderMarkdown = (
   textSize: string = "text-base"
 ): JSX.Element => {
   return (
-    <ReactMarkdown
-      className={`prose dark:prose-invert max-w-full ${textSize}`}
-      components={markdownComponents}
-      remarkPlugins={[remarkGfm, [remarkMath, { singleDollarTextMath: false }]]}
-      rehypePlugins={[[rehypePrism, { ignoreMissing: true }], rehypeKatex]}
-      urlTransform={transformLinkUri}
-    >
-      {content}
-    </ReactMarkdown>
+    <div dir="auto" className={`prose dark:prose-invert max-w-full ${textSize}`}>
+      <ReactMarkdown
+        components={markdownComponents}
+        remarkPlugins={[remarkGfm, [remarkMath, { singleDollarTextMath: false }]]}
+        rehypePlugins={[[rehypePrism, { ignoreMissing: true }], rehypeKatex]}
+        urlTransform={transformLinkUri}
+      >
+        {content}
+      </ReactMarkdown>
+    </div>
   );
 };
 
